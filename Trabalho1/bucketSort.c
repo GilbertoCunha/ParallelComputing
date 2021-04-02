dea4a7f8a8f59985de791680c51b28037d6576b6
Gilberto Cunha <gcacademic@outlook.pt>
2021-04-02 21:05:28 +0100

    refactor bucket distribution and add more tests

--- Trabalho1/bucketSort.c
+++ Trabalho1/bucketSort.c
@@ -1,13 +1,19 @@
-//Compilado em Linux.Sujeito a mudanças caso outro sistema seja utilizado.
- 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
+#include "papi.h"
+
+// Papi defines
+#define NUM_EVENTS 2
+#define NUM_RUNS 5
  
-#define tam_bucket 10000 // Tamanho total do array
-#define num_bucket 10 // Número de baldes
-#define max 1000000 // Maior número que pode ser ordenado
+// Tamanho total do array
+#define totalsize 1000000
+
+// BucketSort defines
+#define num_bucket 10000 // Número de baldes
+#define tam_bucket (int) totalsize/num_bucket // Tamanho total de cada balde
  
 /*
 typedef struct {
@@ -23,9 +29,9 @@ typedef struct {
  
 //cabeçalho das funções
 void bucket_sort (int v[], int tam);
-void bubble (int v[],int tam);
-void bubble1 (int v[],int tam);
-void bubble2 (int v[],int tam);
+void bubble (int v[], int tam);
+void bubble1 (int v[], int tam);
+void bubble2 (int v[], int tam);
 void distributeBuckets (bucket b[], int v[], int tam);
 void distributeBuckets1 (bucket b[], int v[], int tam);
 
@@ -49,35 +55,46 @@ int isOrdered (int v[], int size) {
 
 void bucket_sort (int v[], int tam) {                                     
     bucket b[num_bucket];                                      
-    int i, j, k, aux;                                                 
+    int i, j, k;
+
+    // Inicializar os baldes                                                 
     for(i=0; i<num_bucket; i++) {
-        b[i].topo=0; // Inicializa todos os "topo"
-        b[i].balde = malloc (num_bucket * tam_bucket * sizeof(int)); // Inicializar o array com o tamanho adequado
+        b[i].topo = 0; // Inicializar o número de elementos usados no balde
+        b[i].balde = malloc (totalsize * sizeof(int)); // Inicializar o array com o tamanho adequado
     }
          
     // Distribuir os elementos do array pelos baldes
     // distributeBuckets (b, v, tam);
     distributeBuckets1 (b, v, tam);
          
-    for(i=0; i<num_bucket; i++) //ordena os baldes
-        if(b[i].topo) bubble2 (b[i].balde, b[i].topo);
+    // Ordenar os baldes
+    for(i=0; i<num_bucket; i++)
+        if(b[i].topo) 
+            bubble2 (b[i].balde, b[i].topo);
          
+    // Inserir os elementos ordenados dos baldes de volta no vetor
     i=0;
-    for(j=0; j<num_bucket; j++) { //põe os elementos dos baldes de volta no vetor
-        for(k=0; k<b[j].topo; k++) {
-            v[i]=b[j].balde[k];
-            i++;
-        }
-    }
+    for(j=0; j<num_bucket; j++)
+        for(k=0; k<b[j].topo; k++)
+            v[i++] = b[j].balde[k];
 }
 
 void distributeBuckets (bucket b[], int v[], int tam) {
     int i, j;
+
+    // Calcular máximo e mínimo do array
+    int max = v[0], min = v[0];
+    for (i=1; i<tam; ++i) {
+        if (v[i] > max) max = v[i];
+        if (v[i] < min) min = v[i];
+    }
+
+    // Colocar cada elemento no balde correspondente
     for(i=0; i<tam; i++) {
-        j=(num_bucket)-1;
+        j=num_bucket-1;
         while (1) {
             if(j<0) break;
-            if(v[i]>=j*max) {
+            if(v[i]>=j * ((max - min + 1) / num_bucket)) {
                 b[j].balde[b[j].topo]=v[i];
                 (b[j].topo)++;
                 break;
@@ -89,9 +106,20 @@ void distributeBuckets (bucket b[], int v[], int tam) {
 
 void distributeBuckets1 (bucket b[], int v[], int tam) {
     int i, j, aux;
+    float n;
+
+    // Calcular máximo e mínimo do array
+    int max = v[0], min = v[0];
+    for (i=1; i<tam; ++i) {
+        if (v[i] > max) max = v[i];
+        if (v[i] < min) min = v[i];
+    }
+
+    // Colocar cada elemento no balde correspondente
+    n = (max - min + 1) / num_bucket;
     for(i=0; i<tam; i++) {
         aux = v[i];
-        j = aux / (max / num_bucket);
+        j = aux / n;
         b[j].balde[b[j].topo++] = aux;
     }
 }
@@ -138,19 +166,15 @@ void bubble2 (int v[], int tam) {
         flag=0;
         for(i=0; i<tam-j-1; i++) {
             if(v[i]>v[i+1]) {
-                temp=v[i];
-                v[i]=v[i+1];
-                v[i+1]=temp;
-                flag=1;
+                temp = v[i];
+                v[i] = v[i+1];
+                v[i+1] = temp;
+                flag = 1;
             }
         }
         if(!flag) break;
     }
 }
-
-#include "papi.h"
-#define NUM_EVENTS 2
-#define NUM_RUNS 5
 
 // Add papi metrics
 long long metrics[NUM_EVENTS];
@@ -163,10 +187,8 @@ int EventSet = PAPI_NULL;
 int main () {
 
     // Create and shuffle array
-    int totalsize = tam_bucket * num_bucket;
     int v[totalsize];
-    for (int i=0; i<totalsize; ++i) v[i] = i%max;
-    shuffle (v, totalsize);
+    for (int i=0; i<totalsize; ++i) v[i] = i;
 
     // Make array copy
     int w[totalsize];
@@ -182,6 +204,7 @@ int main () {
     for (int i=0; i<NUM_RUNS; ++i) {
         printf ("Iteration %d: ", i);
         for (int j=0; j<totalsize; ++j) w[j] = v[j];
+        shuffle (w, totalsize);
         
         start = PAPI_get_real_usec();
         PAPI_start(EventSet);
@@ -209,6 +232,7 @@ int main () {
         else printf ("PAPI UNKNOWN EVENT = %.2f\n", means[i]);
     }
     printf ("Execution time: %.2f us\n", texe);
+    printf ("CPI: %.3f\n", means[1]/means[0]);
 
     return 0;
 }
